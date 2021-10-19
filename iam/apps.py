faec11701de6a7ca190b68324a3d526a2fc94b50
Ben <github@ibeni.net>
2021-10-19 11:20:08 +0100

    Remove logic from apps.ready()

--- iam/apps.py
+++ iam/apps.py
@@ -1,23 +1,5 @@
 from django.apps import AppConfig
-from django.db import connections
-
-from .models import Role
-
-
-def table_exists(table_name: str, connection_name: str = 'default') -> bool:
-    return table_name in connections[connection_name].introspection.table_names()
 
 
 class IAMConfig(AppConfig):
     name = 'iam'
-
-    def ready(self):
-        if not table_exists('auth_group'):
-            return
-        from django.apps import apps
-        for app_config in apps.get_app_configs():
-            if rules_module := getattr(app_config.module, 'rules', None):
-                if roles := getattr(rules_module, 'Roles', None):
-                    for attr in dir(roles):
-                        if isinstance(role := getattr(roles, attr), Role):
-                            role.group  # NoQA

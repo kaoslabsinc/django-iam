3a6ab3a68c092c3277a1c7434909ddec4730cee3
Ben <github@ibeni.net>
2021-10-19 12:21:38 +0100

    In refresh_from_db() check self.id before calling Group.objects.get_or_create

--- iam/models.py
+++ iam/models.py
@@ -11,8 +11,9 @@ class Role(Group):
         super().__init__(*args, **kwargs)
 
     def refresh_from_db(self, using=None, fields=None):
-        group, _ = Group.objects.get_or_create(name=self.name)
-        self.id = group.id
+        if not self.id:
+            group, _ = Group.objects.get_or_create(name=self.name)
+            self.id = group.id
         super().refresh_from_db(using=using, fields=fields)
 
     @property

2de95b67185ba048b55dcb523a4fc8df24f720f3
Ben <github@ibeni.net>
2021-10-20 08:57:18 +0100

    Use IAMConfig post_migrate signal instead of AuthConfig

--- iam/signals.py
+++ iam/signals.py
@@ -1,14 +1,14 @@
-import django.contrib.auth.apps
+from django.apps import apps
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
-from iam.models import Role
+from .apps import IAMConfig
+from .models import Role
 
 
 @receiver(post_migrate)
 def create_roles(sender, **kwargs):
-    if isinstance(sender, django.contrib.auth.apps.AuthConfig):
-        from django.apps import apps
+    if isinstance(sender, IAMConfig):
         for app_config in apps.get_app_configs():
             if rules_module := getattr(app_config.module, 'rules', None):
                 if roles := getattr(rules_module, 'Roles', None):

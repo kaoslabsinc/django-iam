6c633ac89498685fbcbca233092ce2bb42785b7f
Keyvan Mosharraf <github@keyvanm.com>
2021-11-02 12:44:22 -0700

    Adjust test to the new code

--- tests/contrib/test_users.py
+++ tests/contrib/test_users.py
@@ -1,6 +1,6 @@
 from django.urls import reverse
 
-from simple.models import SimpleManager, SimpleModel
+from users.rules import Roles as UserRoles
 
 
 def test_AbstractIAMUser(django_user_model):
@@ -9,10 +9,6 @@ def test_AbstractIAMUser(django_user_model):
     assert user.full_name == ""
     assert user.display_name == username
     assert user.initials == "U"
-
-    SimpleManager.objects.create(user=user)
-    add_model_perm = SimpleModel.get_perm('add')
-    assert user.has_perm(add_model_perm)
 
     user.first_name = "User"
     user.last_name = "McUser"
@@ -32,6 +28,8 @@ def test_IAMUserAdmin(client, django_user_model):
                                                       is_superuser=True)
     user_staff_username = 'user_staff'
     user_staff = django_user_model.objects.create(username=user_staff_username, is_staff=True)
+    admin_group = UserRoles.admin.load_group()
+    user_staff.groups.add(admin_group)
 
     opts = django_user_model._meta
     admin_url_changelist = reverse(f'admin:{opts.app_label}_{opts.model_name}_changelist')
@@ -54,7 +52,7 @@ def test_IAMUserAdmin(client, django_user_model):
         }
     )
     assert response.status_code == 302
-    user_superuser = django_user_model.objects.get(username=user_superuser_username)
+    user_superuser.refresh_from_db()
     assert not user_superuser.is_superuser
     user_superuser.is_superuser = True  # Set this back to the original condition for the rest of the test
     user_superuser.save()
@@ -66,7 +64,7 @@ def test_IAMUserAdmin(client, django_user_model):
     response = client.get(admin_url_change_superuser)
     assert response.status_code == 302
 
-    # Check is normal staff can edit is_superuser, a protected attribute
+    # Check if normal staff can edit is_superuser, a protected attribute
     admin_url_change_staff_user = reverse(f'admin:{opts.app_label}_{opts.model_name}_change', args=[user_staff.id])
     response = client.post(
         admin_url_change_staff_user,
@@ -77,5 +75,5 @@ def test_IAMUserAdmin(client, django_user_model):
         }
     )
     assert response.status_code == 302
-    user_staff = django_user_model.objects.get(username=user_staff_username)
+    user_staff.refresh_from_db()
     assert not user_staff.is_superuser

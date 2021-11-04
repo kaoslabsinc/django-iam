b93ffcfd57a389443b180135c2090928e2eb1dbf
Keyvan Mosharraf <github@keyvanm.com>
2021-11-04 13:59:32 -0700

    Implement HasOwnerAdminBlock in example: BlogPostAdmin

--- example/blog/admin.py
+++ example/blog/admin.py
@@ -1,8 +1,30 @@
+from building_blocks.admin.blocks import HasNameAdminBlock, HasDescriptionAdminBlock
 from django.contrib import admin
 
 from iam.contrib.admin.admin import ObjectPermissionsProfileAdmin
+from iam.contrib.admin.blocks import HasOwnerAdminBlock
 from .models import BlogAdminProfile, BlogAuthorProfile, BlogPost
 
 admin.site.register(BlogAdminProfile, ObjectPermissionsProfileAdmin)
 admin.site.register(BlogAuthorProfile, ObjectPermissionsProfileAdmin)
-admin.site.register(BlogPost)
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(
+    admin.ModelAdmin
+):
+    search_fields = (
+        *HasNameAdminBlock.search_fields,
+        *HasOwnerAdminBlock.search_fields
+    )
+    list_display = (
+        *HasNameAdminBlock.list_display,
+        *HasOwnerAdminBlock.list_display
+    )
+    autocomplete_fields = HasOwnerAdminBlock.autocomplete_fields
+    edit_readonly_fields = HasOwnerAdminBlock.edit_readonly_fields
+    fields = (
+        *HasNameAdminBlock.fields,
+        *HasOwnerAdminBlock.fields,
+        *HasDescriptionAdminBlock.fields,
+    )

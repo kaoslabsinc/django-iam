c229c4d6162be66fd00d27ed99fee8cfb6fbc964
Ben <github@ibeni.net>
2021-11-04 10:38:32 +0000

    Remove check_role_groups from regsitry when running migrate command

--- iam/management/commands/migrate.py
+++ iam/management/commands/migrate.py
@@ -1,10 +1,12 @@
 from django.core.management.commands.migrate import Command as MigrateCommand
+from django.core.checks.registry import registry
 
-from iam.checks import Tags
+from iam.checks import Tags, check_role_groups
 
 
 class Command(MigrateCommand):
     skip_checks = [Tags.iam]
 
     def check(self, tags=None, **kwargs):
+        registry.registered_checks.remove(check_role_groups)
         super(Command, self).check(tags=tags, **kwargs)

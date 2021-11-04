60a095a62f62ba403da5152074a4b2b5accb4fa0
Keyvan Mosharraf <github@keyvanm.com>
2021-11-04 13:10:51 -0700

    Refactor methods to get or set roles for users into IAMUserMixin

--- iam/mixins.py
+++ iam/mixins.py
@@ -6,18 +6,25 @@ class IAMUserMixin:
         self._roles = {}  # { ProfileModel: instance | False }
         super(IAMUserMixin, self).__init__(*args, **kwargs)
 
-    def load_roles(self):
-        self._roles = {}
-        for model_cls in get_registered_roles():
-            try:
-                instance = model_cls.objects.active().get(user=self)
-            except model_cls.DoesNotExist:
-                instance = False
-            self.set_role(model_cls, instance)
-
-    def set_role(self, model_class, instance):
-        self._roles[model_class] = instance
-
     @property
     def roles(self):
         return self._roles
+
+    def set_role(self, model_cls):
+        try:
+            profile_instance = model_cls.objects.active().get(user=self)
+        except model_cls.DoesNotExist:
+            profile_instance = False
+        self._roles[model_cls] = profile_instance
+        return profile_instance
+
+    def get_or_set_role(self, model_cls):
+        profile_instance = self.roles.get(model_cls)  # None | False | instance
+        if profile_instance is None:
+            profile_instance = self.set_role(model_cls)
+        return profile_instance
+
+    def load_roles(self):
+        self._roles = {}
+        for model_cls in get_registered_roles():
+            self.set_role(model_cls)

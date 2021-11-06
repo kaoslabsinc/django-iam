b3a9d1a0c5ec26525c959db27ec6283a85fd6afc
Keyvan Mosharraf <github@keyvanm.com>
2021-11-06 16:40:32 -0700

    Test the case for a user who doesn't have the author profile but can still access that page

--- tests/contrib/test_admin.py
+++ tests/contrib/test_admin.py
@@ -1,18 +1,25 @@
 from django.urls import reverse
 
 from blog.models import BlogAuthorProfile, BlogPost
+from users.models import AppAdminProfile
 
 
 def test_AutoOwnerAdminMixin(client, django_user_model):
-    user = django_user_model.objects.create(is_staff=True)
-    BlogAuthorProfile.objects.create(user=user)
-
     opts = BlogPost._meta
     admin_url_add = reverse(f'admin:{opts.app_label}_{opts.model_name}_add')
 
+    user = django_user_model.objects.create(username='user', is_staff=True)
+    BlogAuthorProfile.objects.create(user=user)
     client.force_login(user)
     response = client.get(
         admin_url_add,
     )
-
     assert response.context_data['adminform'].form.fields['owner'].initial.user == user
+
+    user2 = django_user_model.objects.create(username='user2', is_staff=True)
+    AppAdminProfile.objects.create(user=user2)
+    client.force_login(user2)
+    response = client.get(
+        admin_url_add,
+    )
+    assert response.context_data['adminform'].form.fields['owner'].initial is None

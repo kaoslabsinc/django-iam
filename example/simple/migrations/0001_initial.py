d7a0927ae09ea6fa3cc0b3cd9eb4d7d1e9a4e77b
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-10-27 14:08:21 -0400

    Fix up the migration with the perv enhancement

--- example/simple/migrations/0001_initial.py
+++ example/simple/migrations/0001_initial.py
@@ -3,6 +3,8 @@
 from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
+import model_utils.fields
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -18,6 +20,11 @@ class Migration(migrations.Migration):
             name='AuthorProfile',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False,
+                                                                verbose_name='created')),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False,
+                                                                      verbose_name='modified')),
+                ('uuid', models.UUIDField(default=uuid.uuid4, editable=False, unique=True, verbose_name='UUID')),
                 ('is_archived', models.BooleanField(default=False)),
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],

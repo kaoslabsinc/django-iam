9f0cb879b99067690837eccfbc137fd275c1003d
Keyvan Mosharraf <keyvan@keyvanm.com>
2023-02-01 18:20:38 -0500

    Test Profile.get_for_user

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,6 +9,10 @@ def test_UserProfileModel(create_user):
     assert profile.is_available
     assert str(profile) == "username as author profile"
 
+    assert AuthorProfile.get_for_user(user) is not None
+    user2 = create_user()
+    assert AuthorProfile.get_for_user(user2) is None
+
 
 def test_registry():
     registered_roles = get_registered_roles()

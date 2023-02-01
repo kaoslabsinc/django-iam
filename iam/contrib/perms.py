9f0c6bfb7e49c34f9f63558149915a54013c80db
Keyvan Mosharraf <keyvan@keyvanm.com>
2023-02-01 13:48:16 -0500

    Update and improve docstrings

--- iam/contrib/perms.py
+++ iam/contrib/perms.py
@@ -6,6 +6,11 @@ from .predicates import p_system
 class Perms:
     """
     A utility class with common `rules_permissions` (from `rules.RulesModel` interface) values.
+
+    Example:
+        >>> class MyModel(RulesModel):
+        ...     class Meta:
+        ...         rules_permissions = Perms.all_is_staff
     """
     all_is_staff = {
         'add': rules.is_staff,
